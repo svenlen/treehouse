1360780855c9b824dc23d744a84da144ecb70669
Sven Lenaerts <sven@umber.me>
2015-01-31 23:44:09 -0500

    Constants

--- Swift/MyPlayground.playground/section-1.swift
+++ Swift/MyPlayground.playground/section-1.swift
@@ -2,4 +2,12 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var str : String = "Hello, playground"
+
+let modernProgrammingLanguage : String = "Swift"
+
+var greeting = str + modernProgrammingLanguage
+
+// Constants = let. A constant won't be able to has it value changed.
+
+// You can explic specify the type by adding ": [type]" eg. ": String"
