1b97069ee2c9acc5ab139de9f133fa1a294e2a7d
Sven Lenaerts <sven@umber.me>
2015-11-30 19:22:06 +0100

    Dictionary

--- Swift/Swift Basics/Collections.playground/Contents.swift
+++ Swift/Swift Basics/Collections.playground/Contents.swift
@@ -24,3 +24,44 @@ todo.insert("Pay bills", atIndex: 2)
 todo.removeAtIndex(3)
 todo
 todo.count
+
+// Dealing with non-existent data
+
+// let lastItem = todo[5]
+
+// Dictionaries 
+
+/*
+    Airport Code        Airport Name
+    (Key)               (Value)
+
+    LGA                 La Guardia
+    LHR                 Heathrow
+    CDG                 Charles De Gaulle
+    HKG                 Hong Kong International
+    DXB                 Dubai International
+*/
+
+var airportCodes: [String: String] = [
+                    "LGA":"La Guardia",
+                    "LHR":"Heathrow",
+                    "CDG":"Charles De Gaulle",
+                    "HGK":"Hong Kong International",
+                    "DXB":"Dubai International"
+                    ]
+
+// Read from a dictionairy
+
+airportCodes["LGA"]
+airportCodes["SYD"] = "Sydney Airport"
+airportCodes.updateValue("Dublin Airport", forKey: "DUB")
+
+// Removing Key Value Pairs
+
+airportCodes["DXB"] = nil
+airportCodes.removeValueForKey("DXB")
+
+// Dealing with non-existent data
+
+let airportName = airportCodes["LGA"]
+let orlandeAirport = airportCodes["MCO"]
